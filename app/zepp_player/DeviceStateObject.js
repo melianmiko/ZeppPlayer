a42beb24636dd77bd4e7f4c1c736cb81bbc1ae51
MelianMiko <melianmiko@yandex.ru>
2022-07-07 23:04:59 +0700

    Refactor DeviceState object: add more info

--- app/zepp_player/DeviceStateObject.js
+++ app/zepp_player/DeviceStateObject.js
@@ -3,111 +3,171 @@ export function createDeviceState() {
         ALARM_CLOCK: {
             value: "09:30",
             type: "string",
-            floatVal: () => {
+            maxLength: 5,
+            getString: (t) => t.value,
+            getProgress: (t) => {
                 try {
-                    const v = state.ALARM_CLOCK.value.split(":");
-                    return parseInt(v[0]) + (parseInt(v[1]) / 100);
-                } catch(e) { return 0; }
-            },
-            mkString: (v) => {
-                return Math.round(v).toString().padStart(2, "0") + ":" +
-                    Math.round(v % 1 * 100).toString().padStart(2, "0")
-            },
-            getProgress: () => Math.min(1, state.ALARM_CLOCK.floatVal() / 24)
+                    const v = t.value.split(":");
+                    const float = (parseInt(v[0]) + (parseInt(v[1]) / 100));
+                    return Math.min(1 , float / 24);
+                } catch(e) {
+                    return 0;
+                }
+            }
         },
         DISCONNECT: {
             value: false,
             type: "boolean",
-            getProgress: () => state.DISCONNECT.value ? 1 : 0,
-            shift: (tick) => tick % 3 == 0 ? !state.DISCONNECT.value : null
+            maxLength: 5,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value ? 1 : 0,
+            shift: (tick, t) => tick % 3 == 0 ? !t.value : null
         },
         DISTURB: {
             value: true,
             type: "boolean",
-            getProgress: () => state.DISTURB.value ? 1 : 0,
-            shift: (tick) => tick % 2 == 0 ? !state.DISTURB.value : null
+            maxLength: 5,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value ? 1 : 0,
+            shift: (tick, t) => tick % 3 == 0 ? !t.value : null
         },
         LOCK: {
             value: true,
             type: "boolean",
-            getProgress: () => state.LOCK.value ? 1 : 0,
-            shift: (tick) => tick % 3 == 0 ? !state.LOCK.value : null
+            maxLength: 5,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value ? 1 : 0,
+            shift: (tick, t) => tick % 3 == 0 ? !t.value : null
         },
         STEP: {
             value: 4500,
-            getProgress: () => state.STEP.value / state.STEP_TARGET.value,
-            shift: () => (state.STEP.value + 500) % state.STEP_TARGET.value
+            type: "number",
+            maxLength: 5,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / state.STEP_TARGET.value,
+            shift: (_, t) => (t.value + 500) % state.STEP_TARGET.value
         },
         STEP_TARGET: {
-            value: 9000
+            value: 9000,
+            type: "number",
+            maxLength: 5,
+            getString: (t) => t.value.toString()
         },
         DISTANCE: {
             value: 1.5,
+            type: "number",
+            maxLength: 5, // with dot
+            getString: (t) => {
+                const km = Math.floor(t.value).toString(),
+                    dm = Math.round((t.value % 1) * 100).toString();
+                return km.padStart(2, "0") + "." + dm.padStart(2, "0");
+            },
             shift: () => (state.DISTANCE.value + 0.5) % 20
         },
         HEART: {
             value: 99,
-            getProgress: () => state.HEART.value / 180,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 180,
             shift: () => (state.HEART.value + 15) % 180
         },
         CAL: {
             value: 320,
-            getProgress: () => state.CAL.value / state.CAL_TARGET.value,
-            shift: () => (state.CAL.value + 30) % state.CAL_TARGET.value
+            type: "number",
+            maxLength: 4,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / state.CAL_TARGET.value,
+            shift: (_, t) => (t.value + 30) % state.CAL_TARGET.value
         },
         CAL_TARGET: {
-            value: 500
+            value: 500,
+            type: "number",
+            maxLength: 4,
+            getString: (t) => t.value.toString()
         },
         BATTERY: {
             value: 60,
-            getProgress: () => state.BATTERY.value / 100,
-            shift: () => (state.BATTERY.value) % 100 + 10
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
+            shift: (_, t) => (t.value) % 100 + 10
         },
         WEEKDAY: {
             value: 0,
-            getProgress: () => state.WEEKDAY.value / 6,
+            type: "number",
+            maxLength: 1,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 6,
             shift: (tick) => tick % 2 == 0 ? (state.WEEKDAY.value + 1) % 7 : null
         },
         HOUR: {
             value: 9,
-            getProgress: () => state.HOUR.value / 12,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 12,
             shift: (tick) => tick % 2 == 0 ? (state.HOUR.value + 1) % 24 : null
         },
         MINUTE: {
             value: 30,
-            getProgress: () => state.MINUTE.value / 60,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 60,
             shift: () => (state.MINUTE.value + 5) % 60
         },
         SECOND: {
             value: 45,
-            getProgress: () => state.SECOND.value / 60,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 60,
             shift: () => (state.SECOND.value + 1) % 60
         },
         DAY: {
             value: 15,
-            getProgress: () => state.DAY.value / 31,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 31,
             shift: () => (state.DAY.value + 1) % 31
         },
         MONTH: {
             value: 9,
-            getProgress: () => state.MONTH.value / 12,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 12,
             shift: (tick) => tick % 2 == 0 ? state.MONTH.value % 12 + 1 : null
         },
         STAND: {
             value: 12,
-            getProgress: () => state.STAND.value / state.STAND_TARGET.value,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / state.STAND_TARGET.value,
             shift: (tick) => tick % 2 == 0 ? state.STAND.value % state.STAND_TARGET.value + 1 : null
         },
         STAND_TARGET: {
-            value: 13
+            value: 13,
+            type: "number",
+            maxLength: 2,
+            getString: (t) => t.value.toString()
         },
         YEAR: {
-            value: 2022
+            value: 2022,
+            type: "number",
+            maxLength: 4,
+            getString: (t) => t.value.toString().padStart(4, "0")
         },
         AM_PM: {
             value: "hide",
             type: "string",
             info: "AM/PM state: hide - 24h mode, am/pm - 12h mode",
+            maxLength: 4,
+            getString: (t) => t.value,
             shift: () => {
                 if(state.AM_PM == "hidden") return "am";
                 if(state.AM_PM == "am") return "pm";
@@ -116,12 +176,18 @@ export function createDeviceState() {
         },
         PAI_WEEKLY: {
             value: 55,
-            getProgress: () => state.PAI_WEEKLY.value / 100,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
             shift: (tick) => tick % 3 == 0 ? state.PAI_WEEKLY.value % 100 + 4 : null
         },
         PAI_DAILY: {
             value: 80,
-            getProgress: () => state.PAI_DAILY.value / 100,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
             shift: () => state.PAI_DAILY.value % 100 + 4
         },
         WEATHER_CURRENT_ICON: {
@@ -130,40 +196,71 @@ export function createDeviceState() {
         },
         WEATHER_CURRENT: {
             value: 12,
-            getProgress: () => state.WEATHER_CURRENT_ICON.value / 29
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: () => state.WEATHER_CURRENT_ICON.value / 29 //redir to icon
         },
         HUMIDITY: {
             value: 10,
-            getProgress: () => state.HUMIDITY.value / 100,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
             shift: () => state.HUMIDITY.value % 92 + 8
         },
         UVI: {
             value: 10,
-            getProgress: () => state.UVI.value / 100,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
             shift: () => state.UVI.value % 100 + 5
         },
         STRESS: {
             value: 50,
-            getProgress: () => state.STRESS.value / 200,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100,
             shift: () => state.STRESS.value % 100 + 5
         },
         FAT_BURNING: {
             value: 0,
-            getProgress: () => state.FAT_BURNING.value / state.FAT_BURNING_TARGET.value,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / state.FAT_BURNING_TARGET.value,
             shift: () => (state.FAT_BURNING.value + 1) % state.FAT_BURNING_TARGET.value
         },
         FAT_BURNING_TARGET: {
-            value: 20
+            value: 20,
+            type: "number",
+            maxLength: 3
         },
         BODY_TEMP: {
-            value: 36.5
+            value: 36.5,
+            type: "number",
+            maxLength: 4,  // with dot
+            getString: (t) => {
+                const km = Math.floor(t.value).toString(),
+                    dm = Math.round((t.value % 1) * 100).toString();
+                return km.padStart(2, "0") + "." + dm.padStart(1, "0");
+            },
+            getProgress: (t) => t.value / 45
         },
         SPO2: {
             value: 30,
-            getProgress: () => state.SPO2.value / 100
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value / 100
         },
-        SUN_CURRENT: {
+        SUN_CURRENT: { // ???
             value: 0,
+            type: "number",
+            maxLength: 3,
+            getString: (t) => t.value.toString(),
             getProgress: () => state.HOUR.value / 24
         },
         WEAR_STATE: {
@@ -180,9 +277,13 @@ export function createDeviceState() {
         },
         MUSIC_IS_PLAYING: {
             value: true,
-            type: "boolean"
+            type: "boolean",
+            maxLength: 5,
+            getString: (t) => t.value.toString(),
+            getProgress: (t) => t.value ? 1 : 0,
+            shift: (tick, t) => tick % 3 == 0 ? !t.value : null
         },
-        MOON: {
+        MOON: { // ???
             getProgress: () => 0.3
         }
     };
