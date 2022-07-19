d2be3a82fc944462c0b3e09f63548e7c67df0421
MelianMiko <melianmiko@yandex.ru>
2022-07-19 21:18:21 +0700

    Add support of padding prop in TEXT_IMG

--- app/zepp_player/ui/ImagingWidgets.js
+++ app/zepp_player/ui/ImagingWidgets.js
@@ -236,6 +236,8 @@ export class TextImageWidget extends BaseWidget {
         if(config.type) {
             text = player.getDeviceState(config.type, "string");
             maxLength = player.getDeviceState(config.type, "maxLength");
+
+            if(config.padding) text = text.padStart(maxLength, "0");
         }
 
         if(config.text !== undefined) text = config.text;
