7c25b2dcdd2ddb969aaf5f3c290c835377f28da5
MelianMiko <melianmiko@yandex.ru>
2022-07-22 20:26:23 +0700

    Add LICENSE: GPL-v3

--- app/zepp_player/NodeZeppPlayer.js
+++ app/zepp_player/NodeZeppPlayer.js
@@ -1,4 +1,22 @@
-import {createCanvas, loadImage, Image} from "canvas";
+/*
+    ZeppPlayer - ZeppOS, mostly Mi Band 7, simulator for PC
+    Copyright (C) 2022  MelianMiko
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import {createCanvas, loadImage} from "canvas";
 import ZeppPlayer from "./ZeppPlayer.js";
 import TGA from "tga";
 import * as fs from 'fs';
