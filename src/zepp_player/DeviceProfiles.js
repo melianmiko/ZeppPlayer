428c1852d9f82723047567a683a7d9ba2f5fb363
MelianMiko <melianmiko@yandex.ru>
2022-11-04 13:03:44 +0700

    More device profies

--- src/zepp_player/DeviceProfiles.js
+++ src/zepp_player/DeviceProfiles.js
@@ -9,5 +9,22 @@ export const DeviceProfiles = {
         screenWidth: 454,
         screenHeight: 454,
         swapRedAndBlueTGA: true
+    },
+    gtr4: {
+        hasOverlay: true,
+        screenWidth: 466,
+        screenHeight: 466,
+        swapRedAndBlueTGA: true
+    },
+    gtr4mini: {
+        hasOverlay: true,
+        screenWidth: 336,
+        screenHeight: 384
+    },
+    gts4: {
+        hasOverlay: true,
+        screenWidth: 390,
+        screenHeight: 450,
+        swapRedAndBlueTGA: true
     }
 }
